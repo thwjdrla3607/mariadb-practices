aefca855eb9bbd3c79fa559a50ec02ca85d978c6
sso <sojeong7524@gmail.com>
2023-08-30 12:29:16 +0900

    함수 정리

---
+++ sql연습/수학함수.sql
@@ -0,0 +1,32 @@
+--
+-- 수학 함수
+--
+
+-- abs
+select abs(1), abs(-1) from dual;
+
+-- floor
+select floor(3.14), floor(3.9999) from dual;
+
+-- ceil
+select ceil(3.14), ceil(3.9999) from dual;
+
+-- mod
+select mod(10, 3) from dual;
+
+-- round(x):.x에 가장 가까운 정수
+-- round(x, d):.x값 중에 소수점 d 자리에 가장 근접한 실수
+SELECT ROUND(1.498), ROUND(1.498, 1), ROUND(1.498, 0) FROM DUAL;
+
+-- power(x, y): x의 y승
+select power(2, 10), pow(2, 10) from dual;
+
+-- sign(x): 양수 1, 음수 -1, 0이면 1
+select sign(20), sign(-100), sign(0) from dual;
+
+-- greatest(x, y, ....), least(x, y, ....): 최대 최소
+select greatest(10, 40, 20, 50, 30), least(10, 40, 20, 50, 30) from dual;
+select greatest('A', 'C', 'X', 'O', 'N'), least('hello', 'hela', 'hell') from dual;
+ 
+
+ 
